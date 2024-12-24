db83c176fd36f36bbf02cb60dde57a53b803a8f3
premji2024 <premjitv24@gmail.com>
2024-12-24 01:09:40 -0800

    added a file for payload

---
+++ src/test/java/File/Payload.java
@@ -0,0 +1,26 @@
+package File;
+
+public class Payload {
+	
+	public static String AddBooking()
+	{
+		
+		return "{\r\n"
+				+ "  \"bookingid\": 0,\r\n"
+				+ "  \"roomid\": 1,\r\n"
+				+ "  \"firstname\": \"string\",\r\n"
+				+ "  \"lastname\": \"string\",\r\n"
+				+ "  \"depositpaid\": true,\r\n"
+				+ "  \"email\": \"string\",\r\n"
+				+ "  \"phone\": \"stringstrin\",\r\n"
+				+ "  \"bookingdates\": {\r\n"
+				+ "    \"checkin\": \"2024-12-23\",\r\n"
+				+ "    \"checkout\": \"2024-12-23\"\r\n"
+				+ "  }\r\n"
+				+ "}";
+		
+	}
+	
+	
+
+}
